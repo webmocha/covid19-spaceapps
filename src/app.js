3b79657f04037a6ac89947d26a9556c9a79c1393
Aukai Tirrell <aukai.tirrell@gmail.com>
2020-05-30 15:16:48 -0700

    add globe component to app

--- src/app.js
+++ src/app.js
@@ -1,9 +1,10 @@
 import React from "react";
 import ReactDom from "react-dom";
+import Globe from "./globe"; 
 
 const App = () => (
   <div>
-    <h1>Hello World</h1>
+    <Globe />
   </div>
 );
 
