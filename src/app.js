19dc89880b0a0d2cc75ba37ea692d33c22ef0083
Aukai Tirrell <aukai.tirrell@gmail.com>
2020-05-31 16:14:05 -0700

    added graphql query

--- src/app.js
+++ src/app.js
@@ -7,17 +7,23 @@ import Filter from "./components/filter";
 function App() {
   const [currentDate, setCurrentDate] = useState(undefined);
   const [currentType, setCurrentType] = useState()
+  const [ data, setData ] = useState([]);
 
   useEffect(() => {
-    console.log(currentDate)
-    console.log(currentType)
-  }, [currentDate, currentType])
+    fetch(`https://spaceapps.webmocha.com/v1/api/gosat/graphql`, {
+      "method": 'POST',
+      "content-type": "application/json",
+      "body": '{"query": "{ entries @gosat { city, latitude, longitude, date, distance, XCO2, XCH4, AOT0, AOT1, AOT2  } }"}' 
+    })
+    .then(res => res.json())
+    .then(data => setData(data.data.entries))
+  }, [])
 
   return (
     <div>
       <Timeline setDate={setCurrentDate} />
       <Filter setFilterType={setCurrentType} />
-      <Globe />
+      <Globe data={data} />
     </div>
   );
 }
